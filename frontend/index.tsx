d04d13ec25b1a723eeb7a586553531bbb8ff5865
Maksym Turbal <turbalmaxim@gmail.com>
2026-02-27 13:23:23 +0200

    fix: rerun build

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -2,4 +2,13 @@ import { definePlugin } from '@steambrew/client';
 
 export default definePlugin(() => {
 	console.log('CSStats Extension: Frontend plugin initializing...');
+
+	const icon = window.SP_REACT.createElement('div', null, 'CSStats.gg');
+
+	return {
+		icon,
+		onDismount() {
+			console.log('CSStats Extension: Frontend plugin dismounted...');
+		},
+	};
 });
